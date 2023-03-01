c51c1787c915a0ec30a8a9052e3246307583dc39
DevloperTarek <devlopertarek433@gmail.com>
2023-02-28 19:05:32 -0800

    Ecommerce site

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+const tarek = document.querySelector(".mobile");
+const hossain = document.querySelector("#navbar");
+tarek.addEventListener('click', ()=>{
+   hossain.classList.add('nav');
+  
+})
+
+
+
+
